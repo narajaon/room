d7473be2c6642078a02d68b2fef5597bfcdc12d4
narajaon <rajaonarisonfabien@yahoo.fr>
2020-03-01 17:30:01 +0100

    feat: added socket.io server and client

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   console.log('user arrived in /');
-  res.send('<b>A BOILERPLATE</b>');
+  res.status(200).sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(PORT, () => {
+io.on('connection', () => {
+  console.log('SOCKET: USER CONNECTED');
+});
+
+http.listen(PORT, () => {
   console.log(`server is ${PORT} up`);
 });
