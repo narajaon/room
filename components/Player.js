6c7a0c29792d6fa62ef31f44175a2df98bd74bda
narajaon <rajaonarisonfabien@yahoo.fr>
2020-03-04 15:55:34 +0100

    feat: added multi players, remove / add next

--- components/Player.js
+++ components/Player.js
@@ -1,18 +1,67 @@
-import dynamic from 'next/dynamic';
-import React from 'react';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 
-function Player({ src }) {
-  return <iframe
-    id="player"
-    src={src}
-    height="720"
-    width="1280"
-    frameBorder="0"
-    scrolling="no"
-    allowFullScreen="true">
-  </iframe>;
+const Clip = styled.div`
+  height: 200px;
+  width: 400px;
+  background-color: red;
+  margin: 10px;
+  position: absolute;
+  /* should be (Wc - Wp) / 2 */
+  left: -100px;
+  box-shadow: 0px -1px 24px 0px rgba(0,0,0,0.75);
+
+  ${({ isActive }) => isActive && css`
+    background-color: yellow;
+    border: 1px solid black;
+  `}
+
+  ${({ order }) => css`
+    z-index: ${order};
+  `}
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: grey;
+`;
+
+const PreviewWrapper = styled.div`
+  height: 100px;
+  width: 200px;
+  background-color: green;
+  position: relative;
+  margin: 0 20px;
+`;
+
+function Preview({ order = 0, isActive, cb }) {
+  return (
+    <PreviewWrapper>
+      <Clip order={order} isActive={isActive} onClick={cb} />
+    </PreviewWrapper>
+  );
 }
 
-export default dynamic(() => Promise.resolve(Player), {
-  ssr: false,
-});
+function Player() {
+  const [isActive, setIsActive] = useState(2);
+
+  const toggleState = (id) => () => {
+    console.log(id);
+
+    setIsActive(id);
+  };
+
+  return (
+    <Wrapper>
+      <Preview isActive={isActive === 0} cb={toggleState(0)} order={0}/>
+      <Preview isActive={isActive === 1} cb={toggleState(1)} order={1}/>
+      <Preview isActive={isActive === 2} cb={toggleState(2)} order={2}/>
+      <Preview isActive={isActive === 3} cb={toggleState(3)} order={1}/>
+      <Preview isActive={isActive === 4} cb={toggleState(4)} order={0}/>
+    </Wrapper>
+  );
+}
+
+export default Player;
--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,21 @@ import axios from 'axios';
 import Player from '../components/Player';
 
 function Index({ clips }) {
-  return (<>
-    {clips.map(({ embed_url, id }) => <Player key={id} src={embed_url}/>)}
-  </>);
+  return (<Player />);
 }
 
-Index.getInitialProps = async ({ req }) => {
-  const { data: games } = await axios.get(`${req.api_url}games/top?first=2`, {
-    headers: { 'Client-ID': req.client_id },
-  });
+// Index.getInitialProps = async ({ req }) => {
+//   const { data: games } = await axios.get(`${req.api_url}games/top?first=2`, {
+//     headers: { 'Client-ID': req.client_id },
+//   });
 
-  const res = await Promise.all(games.data.map(({ id }) => axios.get(`${req.api_url}clips?game_id=${id}&first=3`, {
-    headers: { 'Client-ID': req.client_id },
-  })));
+//   const res = await Promise.all(games.data.map(({ id }) => axios.get(`${req.api_url}clips?game_id=${id}&first=3`, {
+//     headers: { 'Client-ID': req.client_id },
+//   })));
 
-  const clips = res.reduce((acc, { data }) => [...acc, ...data.data], []);
+//   const clips = res.reduce((acc, { data }) => [...acc, ...data.data], []);
 
-  return { clips };
-};
+//   return { clips };
+// };
 
 export default Index;
