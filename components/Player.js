23b24fa242fa8564833a9481d480e9026c6403e0
narajaon <rajaonarisonfabien@yahoo.fr>
2020-03-17 10:38:34 +0100

    chore: arrow func to func slideCB

--- components/Player.js
+++ components/Player.js
@@ -72,68 +72,70 @@ function Clip({ value, cb, width, height, padding, direction, mult }) {
   );
 }
 
-const slideCB = (el, clips, ref, cb) => async () => {
-  const indexOfClicked = clips.indexOf(el);
+function slideCB(el, clips, ref, cb) {
+  return async () => {
+    const indexOfClicked = clips.indexOf(el);
 
-  if (indexOfClicked === 2) return;
+    if (indexOfClicked === 2) return;
 
-  let direction;
-  if (indexOfClicked > 2) {
-    direction = 'left';
-  } else if (indexOfClicked < 2) {
-    direction = 'right';
-  }
+    let direction;
+    if (indexOfClicked > 2) {
+      direction = 'left';
+    } else if (indexOfClicked < 2) {
+      direction = 'right';
+    }
 
-  const result = slide(clips, direction, paddings[indexOfClicked]);
+    const result = slide(clips, direction, paddings[indexOfClicked]);
 
-  await Promise.all(
-    Array.prototype.map.call(ref.current.children, (elem, index) => {
-      const nextIndex = result.indexOf(clips[index]);
-      const initPos = WIDTH * -(2 - index) * MULT;
-      const nextPos = WIDTH * -(2 - nextIndex) * MULT;
-      const initScale = 1 - paddings[index] * 0.15;
-      const nextScale = 1 - paddings[nextIndex] * 0.15;
+    await Promise.all(
+      Array.prototype.map.call(ref.current.children, (elem, index) => {
+        const nextIndex = result.indexOf(clips[index]);
+        const initPos = WIDTH * -(2 - index) * MULT;
+        const nextPos = WIDTH * -(2 - nextIndex) * MULT;
+        const initScale = 1 - paddings[index] * 0.15;
+        const nextScale = 1 - paddings[nextIndex] * 0.15;
 
-      let opacity = [1, 1];
+        let opacity = [1, 1];
 
-      if (direction === 'left') {
-        if ((index === 0 || index === 1) && paddings[indexOfClicked] === 2) {
-          opacity = [1, 0];
-        } else if (index === 0) {
-          opacity = [1, 0];
+        if (direction === 'left') {
+          if ((index === 0 || index === 1) && paddings[indexOfClicked] === 2) {
+            opacity = [1, 0];
+          } else if (index === 0) {
+            opacity = [1, 0];
+          }
+        } else if (direction === 'right') {
+          if ((index === 3 || index === 4) && paddings[indexOfClicked] === 2) {
+            opacity = [1, 0];
+          } else if (index === 4) {
+            opacity = [1, 0];
+          }
         }
-      } else if (direction === 'right') {
-        if ((index === 3 || index === 4) && paddings[indexOfClicked] === 2) {
-          opacity = [1, 0];
-        } else if (index === 4) {
-          opacity = [1, 0];
-        }
-      }
 
-      const player = elem.animate(
-        {
-          transform: [
-            `translateX(${initPos}px) scale(${initScale}, ${initScale})`,
-            `translateX(${nextPos}px) scale(${nextScale}, ${nextScale})`
-          ],
-          opacity
-        },
-        {
-          duration: 300,
-          delay: 100
-        }
-      );
+        const player = elem.animate(
+          {
+            transform: [
+              `translateX(${initPos}px) scale(${initScale}, ${initScale})`,
+              `translateX(${nextPos}px) scale(${nextScale}, ${nextScale})`
+            ],
+            opacity
+          },
+          {
+            duration: 300,
+            delay: 100
+          }
+        );
 
-      return new Promise(res =>
-        player.addEventListener('finish', () => {
-          res();
-        })
-      );
-    })
-  );
+        return new Promise(res =>
+          player.addEventListener('finish', () => {
+            res();
+          })
+        );
+      })
+    );
 
-  cb(result);
-};
+    cb(result);
+  };
+}
 
 function Player() {
   const [clips, setClips] = useState(elements.slice(0, 5));
